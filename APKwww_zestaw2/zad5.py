60cd7057fa2da052ddaff6f06933a7eadbaad258
vorwinator <80983036+vorwinator@users.noreply.github.com>
2021-10-20 13:28:03 +0200

    Update zad5.py
    dodanie zadania nr 6

--- APKwww_zestaw2/zad5.py
+++ APKwww_zestaw2/zad5.py
@@ -22,3 +22,22 @@ print(Calculator().difference(5, 5))
 print(Calculator().multiply(5, 5))
 print(Calculator().divide(5, 5))
 print(Calculator().divide(5, 0))
+#zad 6
+class ScienceCalculator(Calculator):
+
+    def __init__(self):
+        self
+
+    def power(self, a, b):
+        return a**b
+
+    def invert(self, a):
+        if a == 0:
+            return("error")
+        else:
+            return 1/a
+print('\n')
+print(ScienceCalculator().add(5, 5))
+print(ScienceCalculator().power(5, 5))
+print(ScienceCalculator().invert(5))
+print(ScienceCalculator().invert(0))
